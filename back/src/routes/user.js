dd6441a890d6daca63df31e23377b6c16ac0f72f
Sergio <sergio.arriagada.g@mail.pucv.cl>
2023-10-02 16:50:58 -0300

    Se implementa middleware

--- back/src/routes/user.js
+++ back/src/routes/user.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const user = Router();
-const {registrarUsuario,loginUsuario} = require('../controllers/user.controller')
+const {registrarUsuario,loginUsuario,guardarFoto} = require('../controllers/user.controller')
+const userExtractor = require('../middlewares/userExtractor')
 
 
 user.post('/registro', registrarUsuario);
 user.post('/login',loginUsuario);
+user.post('/guardarFoto',userExtractor,upload.single("myFile"),guardarFoto);
 
 module.exports = user
